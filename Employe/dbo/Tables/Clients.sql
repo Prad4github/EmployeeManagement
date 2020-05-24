16193b870a68d4377269e1d34a0ad9022f416230
Prad4github <39903989+Prad4github@users.noreply.github.com>
2020-05-24 14:39:11 +0530

    Adding SQlsolution

---
+++ Employe/dbo/Tables/Clients.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Clients]
+(
+[ClientId] uniqueidentifier NOT NULL default newid(),
+[ClientName] NVARCHAR (200) NOT NULL,
+[Alias] NVARCHAR (1000) NULL,
+[Email] NVARCHAR (1000) NULL,
+[FirstName] NVARCHAR (1000) NULL,
+[LastName] NVARCHAR (1000) NULL,
+[ManagerId] uniqueidentifier NULL,
+[Level] INT NOT NULL,
+CONSTRAINT [PK_Domain.Clients] PRIMARY KEY CLUSTERED([ClientId] ASC),
+CONSTRAINT client_fk FOREIGN KEY([ManagerId])
+        REFERENCES [Managers]([ManagerId])
+        ON Delete CASCADE 
+)
---
+++ Employe/dbo/Tables/Managers.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Managers]
+(
+[ManagerId] uniqueidentifier NOT NULL default newid(),
+[ManagerName] NVARCHAR (200) NOT NULL,
+[Email] NVARCHAR(1000) NULL,
+[Alias] NVARCHAR (1000) NULL,
+[FirstName] NVARCHAR (1000) NULL,
+[LastName] NVARCHAR (1000) NULL,
+[Positon] Int NOT NULL
+CONSTRAINT [PK_Domain.Managers] PRIMARY KEY CLUSTERED ([ManagerId] ASC)
+)
